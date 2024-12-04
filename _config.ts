f28e146e2ccfff0bf21d7e46ae97bdc1d3a01b19
tabbbywright <tabby@paperstarstudios.moe>
2024-12-04 06:59:10 -0500

    First commit

---
+++ _config.ts
@@ -0,0 +1,14 @@
+import lume from "lume/mod.ts";
+import me from "me/mod.ts";
+
+const site = lume({
+    //location: new URL("https://example.com"),
+    server: {
+        //page404: "./not-found.html",
+        open: true,
+  },
+});
+
+site.use(me());
+
+export default site;
